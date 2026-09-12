2deef0b80e2e769bb542784170af4034d036466d
Jeffrey Young <jcyoung@usc.edu>
2026-09-12 08:44:33 +0000

    part1split

--- split.cpp
+++ split.cpp
@@ -16,8 +16,23 @@ the function below should be the only one in this file.
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  if(in == nullptr){
+    odds = nullptr;
+    evens = nullptr;
+    return;
+  }
+  if(in->value % 2 == 0)
+  {
+    evens = in;
+    in = in->next;
+    split(in, odds, evens->next);
+  }
+  else
+  {
+    odds = in;
+    in = in->next;
+    split(in, odds->next, evens);
+  }
 }
 
 /* If you needed a helper function, write it here */
